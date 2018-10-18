92958475e2381d5d3d7fe46c78cb02469a82dc39
xtareq <xtrinsic96@gmail.com>
2018-10-18 07:09:57 +0100

    filemaneger implement

--- resources/views/xdmin/posts/create.blade.php
+++ resources/views/xdmin/posts/create.blade.php
@@ -34,6 +34,12 @@
 
 
     <script>
-        CKEDITOR.replace( 'cks' );
+          var options = {
+    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+  };
+        CKEDITOR.replace( 'cks', options );
     </script>
 @endpush
